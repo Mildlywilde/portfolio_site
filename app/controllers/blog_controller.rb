98c8060abde3c3434daa461052f7c7d1563d0c6a
Jack WIlde <jackwilde630@gmail.com>
2016-12-07 13:39:32 -0500

    configure blogger api

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,7 +1,7 @@
 class BlogController < ApplicationController
 
   def index
-    @posts = HTTParty.get(blog_url)
+    @posts = HTTParty.get("https://www.googleapis.com/blogger/v3/blogs/3387648302045427257?key=#{Figaro.env.blogger_api_key}")
     render 'index'
   end
 
